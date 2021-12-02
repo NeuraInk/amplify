2cd9daee727fa21e0b4ce835e697a434fe0181df
fs2694 <fs2694@tc.columbia.edu>
2021-12-01 23:51:30 -0500

    Fix app.js file conflict

--- src/App.js
+++ src/App.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState, useEffect } from 'react';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
-import { css } from '@emotion/css';
-import { Auth } from 'aws-amplify';
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-import { listNotes } from './graphql/queries';
-import {
-  createNote as createNoteMutation,
-  deleteNote as deleteNoteMutation,
-} from './graphql/mutations';
-import { API, Storage } from 'aws-amplify';
-import Profile from './pages/Profile/Profile';
-import Home from './pages/Home/Home';
-import MyAlbum from './pages/MyAlbum/MyAlbum';
-import UploadTemp from './pages/UploadTemp/UploadTemp';
-import SocialGallery from './pages/SocialGallery/SocialGallery';
-import Setting from './pages/Setting/Setting';
-import Landing from './pages/LandingPage/LandingPage';
-import Upload from './pages/Upload/Upload';
-=======
 import React, { useCallback, useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Auth, Hub } from "aws-amplify";
@@ -35,7 +6,6 @@ import "react-toastify/dist/ReactToastify.css";
 import Routes from "routes";
 import { loginUser } from "components/auth/slice";
 import FallbackSpinner from "components/fallbackSpinner";
->>>>>>> 2460fdc623b6ca1e3e55f32f517a8f5b6229c47f
 
 const App = () => {
   const dispatch = useDispatch();
@@ -70,65 +40,6 @@ const App = () => {
     checkStatus();
   }, [checkAuthStatus]);
 
-<<<<<<< HEAD
-  return (
-    <div className='App'>
-      <Router>
-        <div>
-          <>
-            <Navbar className='nav-bar' variant='dark'>
-              <Navbar.Brand href='/'>NeuraInk</Navbar.Brand>
-              <Nav className='mr-auto'>
-                <Nav.Link href='upload'>Upload</Nav.Link>
-                <Nav.Link href='myalbum'>Album</Nav.Link>
-                <Nav.Link href='social-gallery'>Gallery</Nav.Link>
-                <Nav.Link href='setting'>Setting</Nav.Link>
-                {user ? (
-                  <Nav.Link
-                    onClick={() => {
-                      signout();
-                    }}
-                  >
-                    Sign Out
-                  </Nav.Link>
-                ) : (
-                  <Nav.Link href='upload'>Log In</Nav.Link>
-                )}
-              </Nav>
-            </Navbar>
-          </>
-
-          <Switch>
-            {/* <Route exact path='/'>
-              <Redirect to='/upload' />
-            </Route> */}
-            <Route path='/upload'>
-              <Upload />
-            </Route>
-            <Route path='/myalbum'>
-              <MyAlbum />
-            </Route>
-            <Route path='/social-gallery'>
-              <SocialGallery />
-            </Route>
-            <Route path='/setting'>
-              <Setting />
-            </Route>
-            <Route path='/aboutus'>
-              <Profile />
-            </Route>
-            <Route path='/users'>{/* <Users /> */}</Route>
-            <Route path='/'>
-              <Landing />
-            </Route>
-            <Route path='/'>
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
-=======
   // listen for auth change events
   Hub.listen("auth", async (data) => {
     if (data && data.payload && data.payload.event === "signIn") {
@@ -143,7 +54,6 @@ const App = () => {
       <Routes />
       <ToastContainer />
     </>
->>>>>>> 2460fdc623b6ca1e3e55f32f517a8f5b6229c47f
   );
 };
 
